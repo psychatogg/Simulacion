b726db9dab9fb224743c95ce112e020d16489a12
ajulianaq <115423966+ajulianaq@users.noreply.github.com>
2023-02-09 13:00:55 +0100

    2 listo

--- Ejercicios/simulacion_est/simulacion_est.R
+++ Ejercicios/simulacion_est/simulacion_est.R
@@ -4,8 +4,11 @@
 ## 3. Muestra que rf genera distribuciones F que coinciden con la teórica
 ## 4. Muestra que rchisq genera distribuciones chisq que coinciden con la teórica
 ## 5. Muestra como el valor empírico y el teórico se aproximan al aumentar el n en el caso de tirar 5 dados
-##	y que salga el 1.
-
+## y que salga el 1.
+## 6. Muestre el teorema central del límite con la distribución F. Como F tiene gl1 y gl2, para no hacer
+## inﬁnidad de gráﬁcos, haga la demostración para gl1 = gl2, es decir, siempre los mismos grados de
+## libertad en el numerador y el denominador.
+## 7. Muestre el teorema central del límite con la distribución χ2.
 set.seed(1)
 
 ## 1 
@@ -26,6 +29,22 @@ d <- density(un_generador)
 plot(d) # plot d
 lines(un_eq~x, col="green")
 
+## 2
+x <- seq(-10, 10, .1)
+t_generador <- rt (10000,99)
+
+
+t <- function(x, df){
+	n = ((gamma((df+1)/2))/(sqrt(df*pi)*gamma(df/2))) * (1+(x^2)/df)^((-df+1)/2)
+}
+
+t_eq <- t(x,99)
+
+d <- density(t_generador) 
+plot(d) # plot d
+lines(t_eq~x, col="green")
+
+
 
 
 
@@ -36,4 +55,5 @@ dbinom(1,5,(1/6))
 
 
 
-## 
+## 6
+
