67040784bb141d31081333725c28f97a5630d7b6
ajulianaq <115423966+ajulianaq@users.noreply.github.com>
2023-02-11 12:40:26 +0100

    terminado

--- Ejercicios/simulacion_est/simulacion_est.R
+++ Ejercicios/simulacion_est/simulacion_est.R
@@ -31,19 +31,48 @@ lines(un_eq~x, col="green")
 
 ## 2
 x <- seq(-10, 10, .1)
-t_generador <- rt (10000,99)
+t_generador <- rt (10000,40)
 
 
 t <- function(x, df){
 	n = ((gamma((df+1)/2))/(sqrt(df*pi)*gamma(df/2))) * (1+(x^2)/df)^((-df+1)/2)
 }
 
-t_eq <- t(x,99)
+t_eq <- t(x,40)
 
 d <- density(t_generador) 
 plot(d) # plot d
 lines(t_eq~x, col="green")
 
+## 3 
+x <- seq(-10, 10, .1)
+f_generador <- rf (10000,10,10)
+
+f <- function(x, df1,df2){
+	n = (1 / (x * beta((df1/2), (df2/2)))) * sqrt((((df1*x)^df1) * df2^df2)/((df1*x)+df2)^(df1+df2))
+}
+
+f_eq <- f(x,10,10)
+
+d <- density(f_generador) 
+plot(d) # plot d
+lines(f_eq~x, col="green")
+
+
+## 4
+x <- seq(-10, 10, .1)
+chi_generador <- rchisq (10000,10)
+
+
+chi <- function(x, df){
+	n = (((1/((2^(df/2)) * gamma(df/2)))*x^((df/2)-1))*(exp(-x/2)))
+}
+
+chi_eq <- chi(x,10)
+
+d <- density(chi_generador) 
+plot(d) # plot d
+lines(chi_eq~x, col="green")
 
 
 
@@ -59,3 +88,23 @@ plot(x, type="l", xlab="número de repeticiones (n)", xlim = c(3000,4000))
 
 ## 6
 
+f_promedios <- c()
+for(i in 1:100000){
+	f_promedios[i] <- mean(rf(100,40,40))
+}
+hist(f_promedios, freq=F)
+
+## Comprobación
+mean(f_promedios)
+
+
+## 7
+
+chi_promedios <- c()
+for(i in 1:100000){
+	chi_promedios[i] <- mean(rchisq(100,40))
+}
+hist(chi_promedios, freq=F)
+
+## Comprobación
+mean(chi_promedios)
