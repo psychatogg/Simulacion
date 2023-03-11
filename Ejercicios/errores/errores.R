b2ee6e7e943ff5eaa4aef599dc24b2a531f38b9c
ajulianaq <115423966+ajulianaq@users.noreply.github.com>
2023-03-11 09:36:44 +0100

    5

--- Ejercicios/errores/errores.R
+++ Ejercicios/errores/errores.R
@@ -42,16 +42,219 @@ image(map)
 ## 2
 ## Normal
 parms <- JohnsonFit(c(0, 1, 0, 3),moment="use")
-x <- rJohnson(1000,parms)
+x <- rJohnson(10000,parms)
 plot(density(x))
 
 ## Whitelist
-comp <- which(map == 1,arr.ind = T)
+comp <- as.data.frame(which(map == 1,arr.ind = T))
 
 ## Platicurtica
 parms <- JohnsonFit(c(0, 1, sk[21], kt[12]), moment="use")
-x <- rJohnson(1000,parms)
+x <- rJohnson(10000,parms)
 plot(density(x))
+
+## Mesocurtica
+
+## No podemos, ya que no tenemos una combinación posible con curtosis = 0
+
+## As positiva
+min(comp$row)
+comp[comp$row == 9,]
+parms <- JohnsonFit(c(0, 1, sk[9], kt[57]), moment="use")
+x <- rJohnson(10000,parms)
+plot(density(x))
+
+## As negativa
+max(comp$row)
+comp[comp$row == 33,]
+parms <- JohnsonFit(c(0, 1, sk[33], kt[61]), moment="use")
+x <- rJohnson(10000,parms)
+plot(density(x))
+
+## 3
+
+## Uniforme
+
+set.seed(1)
+
+N=1000 # tamano de la poblacion
+n = 25 # tamano de las muestras
+k = 25000
+
+# creamos una poblacion de N numeros aleatorios con media 5 y sd =2
+poblacion <- runif(N, 0, 1)
+
+# comprobamos media y sd porque al ser aleatorios no tienen que ser exactamente 5 y 2
+mu.pob <- mean(poblacion)
+sd.pob <- sd(poblacion)
+
+# extraigo k muestras
+# no es el codigo mas eficiente pero al menos reservo la memoria al principio
+means <- vector(length=k)
+
+for (i in 1:k){
+	muestra <- sample(poblacion, n)
+	means[i] <- mean(muestra)
+}
+
+# dibujamos esa distribucion de medias
+# plot(density(means)) mas bonito pero menos preciso
+hist(means)
+
+
+# Deben coincidir las afirmaciones teoricas
+m.teo <- mu.pob
+sd.teo <- sd.pob/sqrt(n)
+
+m.emp <- mean(means)
+sd.emp <- sd(means)
+
+sprintf("Media teoría: %.2f -- Media empírica : %.2f", m.teo, m.emp)
+
+## [1] "Media teoría: 4.98 -- Media empírica : 4.98"
+sprintf("Sd teoría: %.2f -- Sd empírica : %.2f", sd.teo, sd.emp)
+
+
+
+
+
+## Chi sq
+
+
+set.seed(1)
+
+N=1000 # tamano de la poblacion
+n = 25 # tamano de las muestras
+k = 25000
+
+# creamos una poblacion de N numeros aleatorios con media 5 y sd =2
+poblacion <- rchisq(N, 3)
+
+# comprobamos media y sd porque al ser aleatorios no tienen que ser exactamente 5 y 2
+mu.pob <- mean(poblacion)
+sd.pob <- sd(poblacion)
+
+# extraigo k muestras
+# no es el codigo mas eficiente pero al menos reservo la memoria al principio
+means <- vector(length=k)
+
+for (i in 1:k){
+	muestra <- sample(poblacion, n)
+	means[i] <- mean(muestra)
+}
+
+# dibujamos esa distribucion de medias
+# plot(density(means)) mas bonito pero menos preciso
+hist(means)
+
+
+# Deben coincidir las afirmaciones teoricas
+m.teo <- mu.pob
+sd.teo <- sd.pob/sqrt(n)
+
+m.emp <- mean(means)
+sd.emp <- sd(means)
+
+sprintf("Media teoría: %.2f -- Media empírica : %.2f", m.teo, m.emp)
+
+## [1] "Media teoría: 4.98 -- Media empírica : 4.98"
+sprintf("Sd teoría: %.2f -- Sd empírica : %.2f", sd.teo, sd.emp)
+
+
+
+## 4
+## Pequeña
+set.seed(1)
+
+N=1000 # tamano de la poblacion
+n = 5 # tamano de las muestras
+k = 25
+
+# creamos una poblacion de N numeros aleatorios con media 5 y sd =2
+poblacion <- rbinom(N, 1, 0.3)
+
+# comprobamos media y sd porque al ser aleatorios no tienen que ser exactamente 5 y 2
+mu.pob <- mean(poblacion)
+sd.pob <- sd(poblacion)
+
+# extraigo k muestras
+# no es el codigo mas eficiente pero al menos reservo la memoria al principio
+means <- vector(length=k)
+
+for (i in 1:k){
+	muestra <- sample(poblacion, n)
+	means[i] <- mean(muestra)
+}
+
+# dibujamos esa distribucion de medias
+# plot(density(means)) mas bonito pero menos preciso
+hist(means)
+
+
+# Deben coincidir las afirmaciones teoricas
+m.teo <- mu.pob
+sd.teo <- sd.pob/sqrt(n)
+
+m.emp <- mean(means)
+sd.emp <- sd(means)
+
+sprintf("Media teoría: %.2f -- Media empírica : %.2f", m.teo, m.emp)
+
+## [1] "Media teoría: 4.98 -- Media empírica : 4.98"
+sprintf("Sd teoría: %.2f -- Sd empírica : %.2f", sd.teo, sd.emp)
+
+
+
+## Grande
+set.seed(1)
+
+N=1000 # tamano de la poblacion
+n = 25 # tamano de las muestras
+k = 25
+
+# creamos una poblacion de N numeros aleatorios con media 5 y sd =2
+poblacion <- rbinom(N, 1, 0.3)
+
+# comprobamos media y sd porque al ser aleatorios no tienen que ser exactamente 5 y 2
+mu.pob <- mean(poblacion)
+sd.pob <- sd(poblacion)
+
+# extraigo k muestras
+# no es el codigo mas eficiente pero al menos reservo la memoria al principio
+means <- vector(length=k)
+
+for (i in 1:k){
+	muestra <- sample(poblacion, n)
+	means[i] <- mean(muestra)
+}
+
+# dibujamos esa distribucion de medias
+# plot(density(means)) mas bonito pero menos preciso
+hist(means)
+
+
+# Deben coincidir las afirmaciones teoricas
+m.teo <- mu.pob
+sd.teo <- sd.pob/sqrt(n)
+
+m.emp <- mean(means)
+sd.emp <- sd(means)
+
+sprintf("Media teoría: %.2f -- Media empírica : %.2f", m.teo, m.emp)
+
+## [1] "Media teoría: 4.98 -- Media empírica : 4.98"
+sprintf("Sd teoría: %.2f -- Sd empírica : %.2f", sd.teo, sd.emp)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
