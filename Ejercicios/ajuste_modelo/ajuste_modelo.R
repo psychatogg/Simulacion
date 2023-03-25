2b397669137e0dd891dee07b5ea56be0556e8228
ajulianaq <115423966+ajulianaq@users.noreply.github.com>
2023-03-25 18:10:53 +0100

    Genera Lms con todas las combos de 1

--- Ejercicios/ajuste_modelo/ajuste_modelo.R
+++ Ejercicios/ajuste_modelo/ajuste_modelo.R
@@ -3,18 +3,22 @@ rm(list=ls())
 load("E:\\GDrive1\\Uni\\Master\\simulacion\\ejercicios\\ajuste_modelo\\auto-mpg.Rda")
 
 
-preds <- names(data_test[,-1])
 
-comb_preds <- combn(preds,5)
 
 miFit <- function(data,dependiente) {
 	formulas <- list()
-	MSEs <- rep(NA, ncol(comb_preds))
 	modelos <- list()
+	preds <- names(data[,-1])
+	comb_preds <- list()
 	
-	## 5 predictores
-	for (i in 1:ncol(comb_preds)) {
-		formulas[[i]] <- formula(paste(dependiente, paste(comb_preds[,i], collapse = " + "), sep = " ~ "))
+	## Genera todas las combinaciones de 1 a 5 predictores
+	for(i in 1:5) {
+		comb_preds[[i]] <- combn(preds,i)
+	}
+	## Genera Lms con todas las combos de 1
+	comb_preds1 <- comb_preds[[1]]
+	for (i in 1:length(comb_preds1)) {
+		formulas[[i]] <- formula(paste(dependiente, paste(comb_preds1[i], collapse = " + "), sep = " ~ "))
 		modelos[[i]] <- lm(formulas[[i]], data)
 	}
 	return(modelos)
@@ -24,13 +28,33 @@ miFit(data_train,"mpg")
 
 
 
-####### PRUEBAS ######
-formulas <- formula(paste("mpg", paste(comb_preds[,1], collapse = " + "), sep = " ~ "))
-																					
-modelo <- lm(formulas,data_test)
+####### PRUEBAS #####
+
+########### 5 PREDICTORES
+miFit <- function(data,dependiente) {
+	preds <- names(data[,-1])
+	comb_preds <- combn(preds,5)
+	formulas <- list()
+	MSEs <- rep(NA, ncol(comb_preds))
+	modelos <- list()
+	
+	
+	## 5 predictores
+	for (i in 1:ncol(comb_preds)) {
+		formulas[[i]] <- formula(paste(dependiente, paste(comb_preds[,i], collapse = " + "), sep = " ~ "))
+		modelos[[i]] <- lm(formulas[[i]], data)
+	}
+	return(modelos)
+}
+##################################
 
 
 
+preds <- names(data_train[,-1])
+comb_preds <- list()
+for(i in 1:5) {
+	comb_preds[[i]] <- combn(preds,i)
+}
 
 	
 
